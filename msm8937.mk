2fcc93195917f46420962de9bf04daa80ec84aa7
Michael Wright <michaelwr@google.com>
2017-01-31 18:34:15 +0000

    msm8937-common: Custom Vibrator implementation
    Add support for new amplitude controls.
    
    Test: Manual testing with vendor/google_experimental/users/michaelwr/Vibrator
          & adb shell /data/nativetest/vibrator_hidl_hal_test/vibrator_hidl_hal_test
    Change-Id: I7e8c489169d2955d3c5fcc7e6aa0e9aa12bf88a2

--- msm8937.mk
+++ msm8937.mk
@@ -374,8 +374,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.msm8937
 
 # Wifi
 PRODUCT_PACKAGES += \
