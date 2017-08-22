18bd2999922090472f125468113bc6d54d4d7665
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:14:14 +0200

    hannah: Build Sensors HIDL multiHAL
    Change-Id: I42e8890f33f925b16c55169766acfef02034a0c5

--- device.mk
+++ device.mk
@@ -240,6 +240,10 @@ PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
 # Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
