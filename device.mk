b88e5a229cbc2cffed985d0d86f22ae05d8ac9f9
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:09:54 +0200

    hannah: Build Lights HIDL HAL interface
    Change-Id: I6ce412afa476924d7c0bd270fe7ca341873d2680

--- device.mk
+++ device.mk
@@ -135,6 +135,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-egis.idc:$(TARGET_COPY_OUT_VENDOR)/idc/uinput-egis.idc
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
