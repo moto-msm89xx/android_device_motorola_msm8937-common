18621016372318c8ef18c5a7a606785dfb0d828d
Luca Stefani <luca020400@lineageos.org>
2017-08-24 01:43:10 +0800

    hannah: Build DRM HIDL HAL interface
    Change-Id: I10d5a1cd3a09be132f56fe59a2667236df3cb594

--- device.mk
+++ device.mk
@@ -106,6 +106,11 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
