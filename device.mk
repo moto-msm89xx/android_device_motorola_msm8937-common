8d682cbf46099e18de82884fa8f234ca1f5c8769
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-27 23:39:54 +0200

    hannah: Add VNDK HALs
    Change-Id: I1723d5531674b1d84c711a655f6476544bb0a956

--- device.mk
+++ device.mk
@@ -343,6 +343,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
