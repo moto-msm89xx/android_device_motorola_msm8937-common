3d1f5f7540daf363ed655ae44ce2b5f44f260462
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-02 16:05:51 -0500

    hannah: Set IMS to use CodeAurora extension
    Change-Id: I64cba319986748e4dad206ca664ccda7eefb348a

--- device.mk
+++ device.mk
@@ -134,6 +134,10 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# IMS
+PRODUCT_PACKAGES += \
+    ims-ext-common
+
 # IRQ balance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
