0000f6dfd54c758de3ea0492679cf4456f5cbc78
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-07 14:01:18 -0800

    hannah: Add QTI privileged app permissions
    Change-Id: Ide1b79d83624e8aba2ee17979efacc356dfacb55

--- device.mk
+++ device.mk
@@ -238,6 +238,7 @@ PRODUCT_PACKAGES += \
 
 # Qualcomm broadcast whitelist
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 # RIL
