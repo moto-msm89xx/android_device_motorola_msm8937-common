ca1581143d3d82735f0f083aa7c5865f47a1bc7e
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-08 19:21:59 -0800

    hannah: Build RCS and telephony-ext
    Change-Id: I456aa2db554647f4f6303964f7fb071f98b923ac

--- device.mk
+++ device.mk
@@ -261,6 +261,13 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.syspart_fixup.sh
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
 # RenderScript
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
@@ -286,6 +293,13 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Vibrator
 PRODUCT_PACKAGES += \
