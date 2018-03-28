58927d3a02e326a0407179875ff5cbedabfc1f8a
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-28 12:24:04 -0700

    hannah: Move sysconfig XMLs to vendor
    Change-Id: I67edb4a3c43444d442b4c859082c7610fa6dae39

--- device.mk
+++ device.mk
@@ -238,8 +238,8 @@ PRODUCT_PACKAGES += \
 
 # Qualcomm broadcast whitelist
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/qti_whitelist.xml
 
 # RIL
 PRODUCT_PACKAGES += \
