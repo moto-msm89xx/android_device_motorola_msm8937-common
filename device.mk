55679d856c5064806023c51cc5b0b34d96baf997
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-08 17:01:00 +0000

    hannah: Build a system version of android.hidl.base@1.0
    Change-Id: Icf4a29d6f8ef1517f4aa1971fc80a748632d8068

--- device.mk
+++ device.mk
@@ -142,7 +142,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
 
 # IMS
 PRODUCT_PACKAGES += \
