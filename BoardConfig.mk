5185480b6b3160768e058a6f6a1544e0b421cdaf
Vinay Verma <vvinay@codeaurora.org>
2018-04-16 07:56:07 +0530

    hannah: Add MATRIX file for vendor HAL's
    Change-Id: I6c7eed54f066e61ad8039eef5ee6081e6de43560

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 TARGET_FS_CONFIG_GEN += \
