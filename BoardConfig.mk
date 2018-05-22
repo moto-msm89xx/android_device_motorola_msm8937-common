73c5f5b59ffca3bfe1b941730d238f590f5b5564
Michael Bestas <mkbestas@lineageos.org>
2018-05-22 19:39:46 +0300

    hannah: Add empty HIDL manifest
    Change-Id: Icd6598c8c6d8221cd5805cd894e3a91fdfdcb114

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,6 +23,9 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=400M
