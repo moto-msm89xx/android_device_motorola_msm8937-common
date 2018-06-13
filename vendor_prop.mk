350849866b7de16109dab83b3918c44913209d7b
Nirmal Abraham <nabrah@codeaurora.org>
2018-06-13 16:46:56 +0530

    hannah: Rename properties as per treble compliance
    CRs-Fixed: 2206282
    Change-Id: Idcd4cec9fdd886388ee539eb1134daada855ad4b

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -77,7 +77,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qualcomm.cabl=0 \
+    ro.vendor.display.cabl=0 \
     ro.sf.lcd_density=320 \
     vendor.display.enable_default_color_mode=1
 
