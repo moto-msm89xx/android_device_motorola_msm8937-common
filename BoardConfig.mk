315e0e2744cde1135fcd4ab7b6329cb16b357da1
TheStrix <parthbhatia98@gmail.com>
2018-09-11 19:31:33 +0530

    hannah: Enable system-as-root
     - Used for non-a/b as well in pie.
    
    Change-Id: Ia5fb1cb6697da999c2bfd356cf7403a62a9e9ee4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,6 +191,11 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
 
+# System as root
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_KERNEL_CMDLINE += skip_initramfs rootwait ro init=/init root=/dev/dm-0
+BOARD_KERNEL_CMDLINE += dm=\"system none ro,0 1 android-verity /dev/mmcblk0p55\"
+
 # Treble
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
