b8dfc6a77854cb8022c7f7a311a0d5307ba01456
Quallenauge <Hamsi2k@freenet.de>
2018-09-27 19:44:14 +0200

    hannah: Add dummy perf profile.
    Since lineageos 15.1 the power hal doesn't use the
    perf_lock_use_profile() method anymore.
    It was used to apply performance profiles.
    On startup the perfd daemon is looking for at least
    one profile which is used as default.
    So there is only a perf-profiles0.conf to make the
    daemon happy.
    
    Removes log spam:
    E ANDR-PERF-MPCTL: Invalid profile no. 0, total profiles 0 only
    
    Change-Id: I170cda016f99ab2ab00fbefe78ddde9757235c44

--- device.mk
+++ device.mk
@@ -246,6 +246,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+
+# Public Libraries
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QMI
