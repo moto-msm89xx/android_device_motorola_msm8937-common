d309e72a8679812f5fb1875f2fed908e8654bc51
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    msm8937-common: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- msm8937.mk
+++ msm8937.mk
@@ -325,6 +325,10 @@ PRODUCT_PACKAGES += \
     libjustshoot_shim \
     libqsapshim
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
