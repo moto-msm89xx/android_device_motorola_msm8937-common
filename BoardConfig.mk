a186919f3c9b88a271ac66d3523f2155cac46e3e
karthick111 <karthickrko61@gmail.com>
2019-03-02 02:13:00 +0530

    hannah: Configure kernel tasks
    Change-Id: I1d0819e249b875859695832c6a70b45b375a9b7b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,16 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 
 # Kernel
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=400M
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x1000000
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
+TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := hannah_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8937
 
