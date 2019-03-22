649f9f8d539510824efa47bbfa644f52af5fe4d9
karthick111 <karthickrko61@gmail.com>
2019-03-22 23:05:39 +0530

    Revert "hannah: Update vndk-sp build mechanism and library set."
    This reverts commit 73b46a79843b5e81eb6b2a86085b49fddd210699.

--- device.mk
+++ device.mk
@@ -350,7 +350,23 @@ PRODUCT_PACKAGES += \
 
 # Vndk-sp
 PRODUCT_PACKAGES += \
-    vndk-sp
+    android.hardware.renderscript@1.0.vndk-sp\
+    android.hardware.graphics.allocator@2.0.vndk-sp\
+    android.hardware.graphics.mapper@2.0.vndk-sp\
+    android.hardware.graphics.common@1.0.vndk-sp\
+    android.hidl.base@1.0.vndk-sp\
+    libhwbinder.vndk-sp\
+    libbase.vndk-sp\
+    libcutils.vndk-sp\
+    libhardware.vndk-sp\
+    libhidlbase.vndk-sp\
+    libhidltransport.vndk-sp\
+    libutils.vndk-sp\
+    libc++.vndk-sp\
+    libsync.vndk-sp\
+    libbacktrace.vndk-sp\
+    libunwind.vndk-sp\
+    liblzma.vndk-sp\
 
 # Wifi
 PRODUCT_PACKAGES += \
