f7f5560dbf1b677b01ebf695f8c1832fe88716e4
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    msm8937-common: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>

--- msm8937.mk
+++ msm8937.mk
@@ -5,6 +5,7 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/motorola/msm8937-common/msm8937-common-vendor.mk)
