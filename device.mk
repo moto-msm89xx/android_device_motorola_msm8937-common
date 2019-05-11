4da109da25418aaf746e566a95bc694e405127f2
karthick111 <karthickrko61@gmail.com>
2019-05-11 08:35:39 +0530

    hannah: Switch arch to arm64
    Change-Id: I37ea264eb3aaa0479872584399ff48f4b7235975

--- device.mk
+++ device.mk
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
 # Get non-open-source specific aspects
