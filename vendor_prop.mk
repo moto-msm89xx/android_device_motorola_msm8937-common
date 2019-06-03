d4ae8f4e3041b183facf071e863cb65a76f2d5a4
karthick111 <karthickrko61@gmail.com>
2019-06-03 18:08:38 +0530

    hannah: Use 2gb Dalvik properties
    Change-Id: I5492a9d6bccef2c6f37e78adf9878128d97ecc6c

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -68,9 +68,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=4m \
     dalvik.vm.heapmaxfree=8m
 
 # Display
