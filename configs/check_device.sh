6a86ac19e21149c89821f6a43a0cac47492ee669
karthick111 <karthickrko61@gmail.com>
2019-06-21 17:13:39 +0530

    hannah: Add back adreno308 Graphic stack
    Change-Id: I3a40373ca58e04a676f6e16466717b8940f8ebc8

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -15,3 +15,20 @@ else
     rm /vendor/etc/mixer_paths_ahannah.xml
     rm /vendor/etc/sensors/sensor_def_qcomdev_ahannah.conf
 fi
+
+    # Replace EGL for rhannah variants
+if [ "$sku" = "XT1924-1" ] || [ "$sku" = "XT1924-4" ] || [ "$sku" = "XT1924-5" ]; then
+    mv /vendor/lib/egl/libEGL_adreno_8917.so /vendor/lib/egl/libEGL_adreno.so
+    mv /vendor/lib/egl/libGLESv1_CM_adreno_8917.so /vendor/lib/egl/libGLESv1_CM_adreno.so
+    mv /vendor/lib/egl/libGLESv2_adreno_8917.so /vendor/lib/egl/libGLESv2_adreno.so
+    mv /vendor/lib64/egl/libEGL_adreno_8917.so /vendor/lib64/egl/libEGL_adreno.so
+    mv /vendor/lib64/egl/libGLESv1_CM_adreno_8917.so /vendor/lib64/egl/libGLESv1_CM_adreno.so
+    mv /vendor/lib64/egl/libGLESv2_adreno_8917.so /vendor/lib64/egl/libGLESv2_adreno.so
+else
+    rm /vendor/lib/egl/libEGL_adreno_8917.so
+    rm /vendor/lib/egl/libGLESv1_CM_adreno_8917.so
+    rm /vendor/lib/egl/libGLESv2_adreno_8917.so
+    rm /vendor/lib64/egl/libEGL_adreno_8917.so
+    rm /vendor/lib64/egl/libGLESv1_CM_adreno_8917.so
+    rm /vendor/lib64/egl/libGLESv2_adreno_8917.so
+fi
