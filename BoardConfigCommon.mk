685a255e098631bc8b1c0761a905690f56369002
Bruno Martins <bgcngm@gmail.com>
2019-09-26 18:19:40 +0000

    msm8937-common: Kill framework compatibility matrix with fire
    This thing is not mandatory at all and only demands extra work by having
    to keep it up-to-date whenever new interfaces are added to the device manifest
    or simply uprev'ed.
    
    Change-Id: I7c5485f201efde86ff1b277951e7faebf8b80437

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,7 +111,6 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(COMMON_PATH)/compatibility_matrix.xml
 TARGET_FS_CONFIG_GEN += \
