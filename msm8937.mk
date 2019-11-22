a4ca1a650572f4a45734dde53d86aaa729b40a5f
LuK1337 <priv.luk@gmail.com>
2019-11-22 15:35:26 +0100

    msm8937-common: Exclude Snap overlays from RRO
    Change-Id: Ibefb7acaa158eea787618d1696057b75d2541e87

--- msm8937.mk
+++ msm8937.mk
@@ -16,7 +16,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
