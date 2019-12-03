d04c5bc97c6bfc4ea825d91a2d854242313ea36a
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-12-02 19:54:12 -0500

    msm8937-common: Update CNE blobs from deen QPKS30.54-22-12
    * Upver vendor.qti.data.factory to v2.0
    * Add vendor.qti.hardware.data.connection v1.1
    * Add vendor.qti.hardware.data.iwlan v1.0
    * Add vendor.qti.hardware.factory v1.1
    
    Change-Id: I1bfb96bd06df49fd3938fc5d3f247e79d9927ec7
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -50,6 +50,10 @@ fi
 function blob_fixup() {
     case "${1}" in
 
+    product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
+        sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
+        ;;
+
     vendor/lib/libjustshoot.so)
         patchelf --add-needed libjustshoot_shim.so "${2}"
         ;;
