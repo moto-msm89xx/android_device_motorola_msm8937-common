ca4dbb10771d60944d8ed8c0fd5c42c2de4b1bd0
kubersharma001 <kubersharma001@gmail.com>
2020-02-11 13:54:59 +0530

    msm8937-common: Add debug WFC prop to force enable it.
    Change-Id: I006154d536c657084dcaa8bca86545bb5e7faf7c

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -77,7 +77,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Debug
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
