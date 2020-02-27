4cde73bcfd33ea24bb839444dee3ea9eb7a6de7e
wh0dat <whodat@remainsilent.net>
2020-02-27 08:24:16 +0530

    hannah: Rework check_device.sh
    Changed the order of if sequences.
    
    Change-Id: I596d53434bf6a5a8c244a1123b050081f10788fc
    Signed-off-by: wh0dat <whodat@remainsilent.net>

--- configs/check_device.sh
+++ configs/check_device.sh
@@ -1,23 +1,23 @@
 #!/sbin/sh
 
-sku=`getprop ro.boot.hardware.sku`
+SKU=`getprop ro.boot.hardware.sku`
 
-if [ "$sku" = "XT1924-1" ] || [ "$sku" = "XT1924-3" ] || [ "$sku" = "XT1924-4" ] || [ "$sku" = "XT1924-5" ]; then
+if [ "$SKU" = "XT1924-7" ] || [ "$SKU" = "XT1924-8" ]; then
+    rm -rf /system/priv-app/MotoDoze
+    rm /vendor/etc/audio_platform_info_ahannah.xml
+    rm /vendor/etc/mixer_paths_ahannah.xml
+    rm /vendor/etc/sensors/sensor_def_qcomdev_ahannah.conf
+else
     mv /vendor/etc/audio_platform_info_ahannah.xml /vendor/etc/audio_platform_info.xml
     mv /vendor/etc/mixer_paths_ahannah.xml /vendor/etc/mixer_paths.xml
     mv /vendor/etc/sensors/sensor_def_qcomdev_ahannah.conf /vendor/etc/sensors/sensor_def_qcomdev.conf
     rm -rf /vendor/app/LineageActions
     rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
     rm /vendor/etc/permissions/android.hardware.sensor.gyroscope.xml
-else
-    rm -rf /system/priv-app/MotoDoze
-    rm /vendor/etc/audio_platform_info_ahannah.xml
-    rm /vendor/etc/mixer_paths_ahannah.xml
-    rm /vendor/etc/sensors/sensor_def_qcomdev_ahannah.conf
 fi
 
     # Replace EGL for rhannah variants
-if [ "$sku" = "XT1924-1" ] || [ "$sku" = "XT1924-4" ] || [ "$sku" = "XT1924-5" ]; then
+if [ "$SKU" = "XT1924-1" ] || [ "$SKU" = "XT1924-4" ] || [ "$SKU" = "XT1924-5" ]; then
     mv /vendor/lib/egl/libEGL_adreno_8917.so /vendor/lib/egl/libEGL_adreno.so
     mv /vendor/lib/egl/libGLESv1_CM_adreno_8917.so /vendor/lib/egl/libGLESv1_CM_adreno.so
     mv /vendor/lib/egl/libGLESv2_adreno_8917.so /vendor/lib/egl/libGLESv2_adreno.so
