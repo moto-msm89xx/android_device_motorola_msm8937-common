367ded28375ca688664e4a3b485905ba004b2f9f
karthick111 <karthickrko61@gmail.com>
2020-02-27 11:08:40 +0530

    hannah: Update zram configuration
    * Set max_comp_streams to 8
    * Set swappiness to 60
    
    Change-Id: I5a39406dd5f991a7ff19feee8697c70145b6959b

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -376,9 +376,9 @@ else
     fi
 
     # Set allocstall_threshold to 0 for all targets.
-    # Set swappiness to 100 for all targets
+    # Set swappiness to 60 for all targets
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
-    echo 100 > /proc/sys/vm/swappiness
+    echo 60 > /proc/sys/vm/swappiness
 
     configure_zram_parameters
 
