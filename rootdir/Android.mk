bec5187a624280b680585c437e8d9f2bcc09cdac
Wiktor Strzębała <wiktorek140@tlen.pl>
2020-02-28 08:45:40 +0100

    msm8937-common: Enable USB configfs and add init script
    Change-Id: I0f7aabb28e0d0f3517f1132208baead0a1bdd739
    Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -38,10 +38,10 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := init.mmi.usb.rc
+LOCAL_MODULE := init.msm.usb.configfs.rc
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := etc/init.mmi.usb.rc
+LOCAL_SRC_FILES := etc/init.msm.usb.configfs.rc
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
