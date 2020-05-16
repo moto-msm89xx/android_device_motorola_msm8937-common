6c46fa75737abccd3411ff7ca1979f5b62619c42
Jarl-Penguin <jarlpenguin@outlook.com>
2020-05-16 18:39:24 +0300

    msm8937-common: Decommonize fingerprint blobs
    * And build the proper fingerprint blob
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Signed-off-by: karthick111 <karthickrko61@gmail.com>
    Change-Id: I356c0243a68d72e618e4b6229653ac949f2c209a

--- msm8937.mk
+++ msm8937.mk
@@ -124,7 +124,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1
 
 # GPS
 PRODUCT_PACKAGES += \
