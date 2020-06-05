6dcf8773e25b8f79055509a9a46b5ddca9ad80d0
Jarl-Penguin <jarlpenguin@outlook.com>
2020-06-05 20:39:09 +0300

    msm8937-common: rootdir: Cleanup
    * /system/vendor -> /vendor
    * /persist -> /mnt/vendor/persist
    * Remove references to services that don't exist
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: If0e3191c158cec9971d91191b5b70525269cf94f

--- rootdir/bin/init.qcom.sensors.sh
+++ rootdir/bin/init.qcom.sensors.sh
@@ -31,14 +31,9 @@
 #
 start_sensors()
 {
-    chmod -h 664 /persist/sensors/sensors_settings
-    chown -h -R system.system /persist/sensors
+    chmod -h 664 /mnt/vendor/persist/sensors/sensors_settings
+    chown -h -R system.system /mnt/vendor/persist/sensors
     start vendor.sensors.qti
-
-    # Only for SLPI
-    if [ -c /dev/msm_dsps -o -c /dev/sensors ]; then
-        start vendor.sensors
-    fi
 }
 
 start_sensors
