8f8c52175ac184e4f0b4df21303d0e2a22248cb5
Jarl-Penguin <jarlpenguin@outlook.com>
2020-08-19 12:08:52 +0300

    msm8937-common: Build LiveDisplay HAL
    * Add it into manifest also
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I263731b7b5ed7bf50b70f36e2d4b6e01d7a08344

--- msm8937.mk
+++ msm8937.mk
@@ -181,6 +181,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageActions
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
