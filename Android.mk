f9c5ebe6090081ffa6c77705d5db74745d6e25a7
Jarl-Penguin <jarlpenguin@outlook.com>
2020-09-11 18:32:02 +0300

    msm8937-common: Add cedric to build guard
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I868bcc0d90aa4a13e90d53b86c14d40db92a8cd5

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hannah james montana,$(TARGET_DEVICE)),)
+ifneq ($(filter cedric hannah james montana,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
