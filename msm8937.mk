c77d17acf6cca31da9f86ac39038f4eb1be1e6ff
Jarl-Penguin <jarlpenguin@outlook.com>
2020-09-11 12:33:28 +0300

    msm8937-common: Uprev camera provider service
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ide5fca09cff3407d8b792a18f2dda902e9f9dae3

--- msm8937.mk
+++ msm8937.mk
@@ -81,8 +81,7 @@ PRODUCT_PACKAGES += \
     libwui
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
+    android.hardware.camera.provider@2.5-service
 
 # CNE
 PRODUCT_PACKAGES += \
