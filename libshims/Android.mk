3521c15ee43ba9ef05b831bddd433821e580d405
dianlujitao <dianlujitao@lineageos.org>
2020-09-13 16:15:39 +0800

    msm8937-common: Provide shim for libril-qc-hal-qmi.so/libril-qc-qmi-1.so
    Change-Id: Ic5d732f3e98ba45c886db9646d6ef30eb0d42bf4

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,6 +16,14 @@ LOCAL_PATH := $(call my-dir)
 
 # RIL
 include $(CLEAR_VARS)
+LOCAL_SRC_FILES := strdup16to8.cpp
+LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_MODULE := libcutils_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libqsap_shim.c
 LOCAL_SHARED_LIBRARIES := libqsap_sdk liblog libcutils libutils
 LOCAL_C_INCLUDES := $(TOP)/system/qcom/softap/sdk
--- msm8937.mk
+++ msm8937.mk
@@ -346,6 +346,7 @@ PRODUCT_PACKAGES += \
 
 # Shims
 PRODUCT_PACKAGES += \
+    libcutils_shim \
     libqsapshim
 
 # Soong namespaces
