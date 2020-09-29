00528bf266607c596eb862240907be78b0d410f7
Jarl-Penguin <jarlpenguin@outlook.com>
2020-09-29 13:58:55 +0300

    msm8937-common: Fix sonames for HALs
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: If4bdb7b06584feb196430088e8953bd90f382a4f

--- extract-files.sh
+++ extract-files.sh
@@ -54,6 +54,26 @@ function blob_fixup() {
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
         ;;
 
+    vendor/lib/hw/activity_recognition.msm8937.so | vendor/lib64/hw/activity_recognition.msm8937.so)
+        patchelf --set-soname activity_recognition.msm8937.so "${2}"
+        ;;
+
+    vendor/lib/hw/camera.msm8937.so)
+        patchelf --set-soname camera.msm8937.so "${2}"
+        ;;
+
+    vendor/lib64/hw/gatekeeper.msm8937.so)
+        patchelf --set-soname gatekeeper.msm8937.so "${2}"
+        ;;
+
+    vendor/lib64/hw/keystore.msm8937.so)
+        patchelf --set-soname keystore.msm8937.so "${2}"
+        ;;
+
+    vendor/lib64/hw/thermal.msm8937.so)
+        patchelf --set-soname thermal.msm8937.so "${2}"
+        ;;
+
     vendor/lib/libmmcamera2_sensor_modules.so)
         sed -i 's|msm8953_mot_deen_camera.xml|msm8937_mot_camera_conf.xml|g' "${2}"
         ;;
