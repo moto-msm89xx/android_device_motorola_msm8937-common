c1fc9e7f2c8cc94b45647b4dea2499d2e8728e7b
Jarl-Penguin <jarlpenguin@outlook.com>
2020-10-02 23:34:41 +0300

    msm8937-common: Commonize HAL3 prop
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ibbf5d8d783e51fd7947dd5a3eab0fd09a322d45a

--- properties.mk
+++ properties.mk
@@ -54,6 +54,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.ts.rtmakeup=true \
     persist.vendor.camera.display.umax=1920x1080 \
     persist.vendor.camera.display.lmax=1280x720 \
+    persist.vendor.camera.HAL3.enabled=1 \
     persist.vendor.qti.telephony.vt_cam_interface=1
 
 # CNE
