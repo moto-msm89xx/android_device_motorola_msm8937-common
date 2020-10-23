83761b96c2d597cb1d2985e88901414796881580
Jarl-Penguin <jarlpenguin@outlook.com>
2020-10-23 10:08:33 +0300

    msm8937-common: Build thermal HAL from source
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I803fc7e87ce770508b5bbc9a98f4a4dc085af9af

--- msm8937.mk
+++ msm8937.mk
@@ -349,7 +349,8 @@ PRODUCT_SOONG_NAMESPACES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.msm8937
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
