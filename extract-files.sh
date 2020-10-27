f255b3db9a104133517f18d465b47add76590665
Erfan Abdi <erfangplus@gmail.com>
2020-10-27 14:05:04 +0300

    msm8937-common: Partially roll back to Pie IMS, QMI & RIL blobs
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ia30250c6fc42e9cf925ede7f9916ee07a46089db

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,10 @@ function blob_fixup() {
         patchelf --remove-needed android.hidl.base@1.0.so "${2}"
         ;;
 
+    product/etc/permissions/com.qualcomm.qti.imscmservice-V2.0-java.xml | product/etc/permissions/com.qualcomm.qti.imscmservice-V2.1-java.xml)
+        sed -i 's|/system/framework|/system/product/framework|g' "${2}"
+        ;;
+
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
         ;;
