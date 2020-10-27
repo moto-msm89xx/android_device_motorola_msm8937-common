0f75332b95273c451b90c2827cc318d363dc4673
Jarl-Penguin <jarlpenguin@outlook.com>
2020-10-27 16:40:15 +0300

    msm8937-common: Decommonize thermal engine config
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ic47a4687465e5036caa5347c3a6b8daebe33fe41

--- msm8937.mk
+++ msm8937.mk
@@ -346,9 +346,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.msm8937
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
