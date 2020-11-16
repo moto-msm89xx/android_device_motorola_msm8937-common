7b24c2d57a113273f385821c10ee85473189d0ae
Quallenauge <Hamsi2k@freenet.de>
2020-11-16 22:16:17 +0100

    [VERIFYME] msm8937-common: Switch to aidl vibrator hal.
    Change-Id: I4dec22449f33bdcd6170759075bdcec5bb715031

--- msm8937.mk
+++ msm8937.mk
@@ -377,8 +377,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl:64 \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
 
 # Wifi
 PRODUCT_PACKAGES += \
