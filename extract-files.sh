56387872c3fd33db4d3167d868f8182c40556b96
Bruno Martins <bgcngm@gmail.com>
2020-11-19 00:32:09 +0000

    msm8837-common: Remove unused jars and radio libs
    Change-Id: I651edb4823e54b589b7b72f6362a07c6e78b955a

--- extract-files.sh
+++ extract-files.sh
@@ -27,10 +27,6 @@ function blob_fixup() {
             "${PATCHELF}" --remove-needed android.hidl.base@1.0.so "${2}"
             ;;
 
-        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
-            sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
-            ;;
-
         # memset shim
         vendor/bin/charge_only_mode)
             "${PATCHELF}" --add-needed libmemset_shim.so "${2}"
