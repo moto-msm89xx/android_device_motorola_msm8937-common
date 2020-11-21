7a678d87d6020f8c73e1932953581773da590e31
Jarl-Penguin <jarlpenguin@outlook.com>
2020-11-21 22:01:22 +0300

    msm8937-common: LineageActions: Nuke dependency on Lineage SDK
    * It isn't needed at all
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I9e8a13c1d23d2d25b5e16ecb39aba734bda82bc4

--- LineageActions/src/org/lineageos/settings/device/BootCompletedReceiver.java
+++ LineageActions/src/org/lineageos/settings/device/BootCompletedReceiver.java
@@ -25,7 +25,6 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.util.Log;
 
-import org.lineageos.internal.util.FileUtils;
 import org.lineageos.settings.device.ServiceWrapper.LocalBinder;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
