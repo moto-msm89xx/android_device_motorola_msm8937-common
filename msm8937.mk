2f4dd414c7418da8127deecd7ddb0ce88c44fb35
Jarl-Penguin <jarlpenguin@outlook.com>
2020-12-15 15:28:14 +0300

    msm8937-common: Move compass permission to ODM
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: If06ce0f41e4900956606aa312c4590bd57ee49a2

--- msm8937.mk
+++ msm8937.mk
@@ -256,7 +256,7 @@ PRODUCT_COPY_FILES += \
 
 ifeq ($(filter ahannah james rhannah,$(TARGET_DEVICE)),)
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_c/android.hardware.sensor.compass.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml
 endif
 
