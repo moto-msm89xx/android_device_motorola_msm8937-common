c42758568c1892e9ae6297057af63e872c2844e5
Jarl-Penguin <jarlpenguin@outlook.com>
2020-12-15 08:43:52 +0300

    msm8937-common: Don't build MotoDoze for all devices
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I33fef6029d4fe0113d3c205eb40365076303ebc9

--- msm8937.mk
+++ msm8937.mk
@@ -100,10 +100,6 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.3 \
     vendor.display.config@1.3.vendor
 
-# Doze
-PRODUCT_PACKAGES += \
-    MotoDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
