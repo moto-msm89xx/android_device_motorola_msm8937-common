a757f95a2557c9af8d85fabda5c8ae35d1ed1a88
Jarl-Penguin <jarlpenguin@outlook.com>
2020-12-16 10:08:13 +0300

    msm8937-common: Retrofit dynamic partitions
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I88e4e83aa50eefe6baf1c28d72077bbdda8d19a8

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -6,11 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := fstab.qcom
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-ifeq ($(PRODUCT_FULL_TREBLE_OVERRIDE), true)
-LOCAL_SRC_FILES := etc/fstab_legacy.qcom
-else
 LOCAL_SRC_FILES := etc/fstab.qcom
-endif
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
@@ -19,11 +15,7 @@ LOCAL_MODULE := fstab_ramdisk.qcom
 LOCAL_MODULE_STEM := fstab.qcom
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
-ifeq ($(PRODUCT_FULL_TREBLE_OVERRIDE), true)
-LOCAL_SRC_FILES := etc/fstab_legacy.qcom
-else
 LOCAL_SRC_FILES := etc/fstab.qcom
-endif
 LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
 
