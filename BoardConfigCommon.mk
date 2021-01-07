28fcf4a07f8d8a38f7ae881bcbb0c23ce3f34560
Wiktor Strzębała <wiktorek140@tlen.pl>
2021-01-07 13:26:58 +0100

    msm8937-common: Set kernel version to 4.9
    Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>
    Change-Id: I863471d019917cad50095d427849bdc991f8baab

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -110,6 +110,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
+TARGET_KERNEL_VERSION := 4.9
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
