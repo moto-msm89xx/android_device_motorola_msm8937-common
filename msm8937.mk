23dc86431121a20b1eb84301147c30f84f2eccc5
Wiktor Strzębała <wiktorek140@tlen.pl>
2021-02-09 19:51:28 +0100

    msm8937-common: Build packages required for modem in 4.9
    Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>
    Change-Id: If004c20f8f114175295a73fcf9b65d155e9768a5

--- msm8937.mk
+++ msm8937.mk
@@ -303,6 +303,10 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     qti_telephony_utils.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.radio.config@1.0 \
+    android.hardware.secure_element@1.0
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
