4242a845fddecfb3b0497d10c5e2d63ca0454340
Jarl-Penguin <jarlpenguin@outlook.com>
2021-07-25 22:00:18 +0300

    [DNM] msm8937-common: Switch back to default AOSP vibrator HAL
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I4422d844d39434ccf8628b89a69aba897ce698ad

--- msm8937.mk
+++ msm8937.mk
@@ -377,7 +377,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-service.msm8937
+    android.hardware.vibrator@1.0-impl:64 \
+    android.hardware.vibrator@1.0-service
 
 # Wifi
 PRODUCT_PACKAGES += \
