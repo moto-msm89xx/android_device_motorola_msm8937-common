c342804a469f094cc4a12d8de33822865739b8fa
Jarl-Penguin <jarlpenguin@outlook.com>
2021-07-25 22:03:45 +0300

    [DNM] msm8937-common: Permissive SELinux
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ic16cd78cc13326a996c7652b367d615df1f2947a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,6 +99,7 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000 vmalloc=400M androidboot.usbconfigfs=true
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/7824900.sdhci
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x1000000
