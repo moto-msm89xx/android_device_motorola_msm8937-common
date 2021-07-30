53cfba326ff006e27f89d308ee8ed51efdb62c71
Jarl-Penguin <jarlpenguin@outlook.com>
2021-07-30 18:39:06 +0300

    msm8937-common: Update thermal blobs from dpl_sprout RKQ1.200928.002
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: Ic4e795b3d60de50e736d4e0d55eba61e60d58613

--- msm8937.mk
+++ msm8937.mk
@@ -363,6 +363,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
