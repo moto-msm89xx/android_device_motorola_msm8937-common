ad4c65ddc742ad4691f7be0ad988f5c71178bb37
Jarl-Penguin <jarlpenguin@outlook.com>
2021-08-12 13:43:12 +0300

    msm8937-common: gps: Update from LA.UM.9.6.2.r1-02800-89xx.0
    * Update GPS configs from dpl_sprout RKQ1.200928.002
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I305ea9eaeff10cb48ba281e66add6c619e90280f

--- gps/synergy_loc_api/Android.mk
+++ gps/synergy_loc_api/Android.mk
@@ -6,16 +6,11 @@ LOCAL_MODULE := libsynergy_loc_api
 LOCAL_SANITIZE += $(GNSS_SANITIZE)
 # activate the following line for debug purposes only, comment out for production
 #LOCAL_SANITIZE_DIAG += $(GNSS_SANITIZE_DIAG)
+
 LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
 LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
 LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
-
-ifeq ($(TARGET_DEVICE),apq8026_lw)
-LOCAL_CFLAGS += -DPDK_FEATURE_SET
-else ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
-LOCAL_CFLAGS += -DPDK_FEATURE_SET
-endif
 
 LOCAL_SHARED_LIBRARIES := \
     libutils \
@@ -42,8 +37,3 @@ LOCAL_HEADER_LIBRARIES := \
 
 LOCAL_CFLAGS += $(GNSS_CFLAGS)
 include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libsynergyloc_api_headers
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
-include $(BUILD_HEADER_LIBRARY)
